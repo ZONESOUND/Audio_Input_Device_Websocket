957021ff14243e062a1aec32fb54ca3e5c4be3ac
lojoyu <sophie200429@gmail.com>
2019-11-06 11:47:17 +0800

    Updates

---
+++ precache-manifest.446582b97f3f9d62d4552b2340fb925b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2796eff3cb37d61e9004be1f4f9dc3",
+    "url": "/voice-control/index.html"
+  },
+  {
+    "revision": "5bac553e3298a42bcfea",
+    "url": "/voice-control/static/css/main.34ce445c.chunk.css"
+  },
+  {
+    "revision": "283a288b6cffe0dbe31d",
+    "url": "/voice-control/static/js/2.14baa739.chunk.js"
+  },
+  {
+    "revision": "5bac553e3298a42bcfea",
+    "url": "/voice-control/static/js/main.f6e492d8.chunk.js"
+  },
+  {
+    "revision": "9b0f56eaf47cb31b4a2d",
+    "url": "/voice-control/static/js/runtime-main.77693d39.js"
+  }
+]);
